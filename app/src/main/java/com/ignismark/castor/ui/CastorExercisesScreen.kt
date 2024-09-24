831b1558499d3b3f59cb5d9b57ca65a64d2e1785
admgk <adamgorz@gmail.com>
2024-09-24 22:27:10 +0200

    Add ExercisesScreen LazyList with mock data

--- app/src/main/java/com/ignismark/castor/ui/CastorExercisesScreen.kt
+++ app/src/main/java/com/ignismark/castor/ui/CastorExercisesScreen.kt
@@ -1,8 +1,57 @@
 package com.ignismark.castor.ui
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun CastorExercisesScreen() {
-
+fun CastorExercisesScreen(paddingValues: PaddingValues) {
+    Surface(modifier = Modifier.padding(paddingValues)) {
+        LazyColumn {
+            items(exercises) { exercise ->
+                ExerciseCard(exercise)
+            }
+        }
+    }
 }
+
+@Composable
+fun ExerciseCard(exercise: Exercise) {
+    Card(onClick = { /*TODO*/ }) {
+        Text(text = exercise.name)
+    }
+}
+
+val exercises: List<Exercise> = listOf(
+    Exercise(
+        name = "Dumbbell Lateral Press",
+        equipment = "Dumbbell",
+        countType = "Weight and Reps",
+        muscleGroup = "Chest"
+    ),
+    Exercise(
+        name = "Push-up",
+        equipment = "Bodyweight",
+        countType = "Reps",
+        muscleGroup = "Chest"
+    ),
+    Exercise(
+        name = "Dumbbell Squat",
+        equipment = "Dumbbell",
+        countType = "Weight and Reps",
+        muscleGroup = "Legs"
+    )
+)
+
+data class Exercise(
+    val name: String,
+    val equipment: String,
+    val countType: String,
+    val muscleGroup: String
+)
