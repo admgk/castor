507823dd58c83971d5122c040c49ec7f4b3d950b
admgk <adamgorz@gmail.com>
2024-09-24 22:11:10 +0200

    Add TopAppBar and NavigationBar

--- app/src/main/java/com/ignismark/castor/ui/CastorApp.kt
+++ app/src/main/java/com/ignismark/castor/ui/CastorApp.kt
@@ -1,12 +1,7 @@
 package com.ignismark.castor.ui
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ignismark.castor.ui.theme.CastorTheme
 
--- app/src/main/java/com/ignismark/castor/ui/CastorNavigationComponents.kt
+++ app/src/main/java/com/ignismark/castor/ui/CastorNavigationComponents.kt
@@ -1,20 +1,86 @@
 package com.ignismark.castor.ui
 
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CastorTopBar(modifier: Modifier = Modifier) {
-    CenterAlignedTopAppBar(title = { /*TODO*/ })
+    CenterAlignedTopAppBar(
+        title = {
+            Text(text = "CASTOR")
+        },
+        navigationIcon =  {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "menu")
+            }
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Filled.AccountCircle,
+                    contentDescription = "account")
+            }
+        },
+        modifier = modifier
+    )
 }
 
 @Composable
-fun CastorBottomNavigation() {
-    BottomAppBar {
-
+fun CastorBottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(
+        modifier = modifier
+    ) {
+        NavigationBarItem(
+            selected = true,
+            onClick = { /*TODO*/ },
+            icon = {
+                Icon(
+                    imageVector = Icons.Filled.DateRange,
+                    contentDescription = "calendar")
+            },
+            label = { 
+                Text(text = "Calendar") 
+            }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = {
+                Icon(
+                    imageVector = Icons.Filled.Edit,
+                    contentDescription = "plan")
+            },
+            label = {
+                Text(text = "Plan")
+            }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.List,
+                    contentDescription = "exercises")
+            },
+            label = {
+                Text(text = "Exercises")
+            }
+        )
     }
 }
