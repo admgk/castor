e255ede65f0ec751e5cc10c8ccf5bafdbfc75b88
admgk <adamgorz@gmail.com>
2025-03-19 15:00:53 +0100

    Add Workout local data provider

--- app/src/main/java/com/ignismark/castor/data/local/LocalExercisesDataProvider.kt
+++ app/src/main/java/com/ignismark/castor/data/local/LocalExercisesDataProvider.kt
@@ -1,25 +1,26 @@
 package com.ignismark.castor.data.local
 
 import com.ignismark.castor.model.Exercise
+import com.ignismark.castor.model.RepsAndWeight
 
 object LocalExercisesDataProvider {
     val exercises: List<Exercise> = listOf(
         Exercise(
             name = "Dumbbell Lateral Press",
             equipment = "Dumbbell",
-            countType = "Weight and Reps",
+            countType = RepsAndWeight(null, null),
             muscleGroup = "Chest"
         ),
         Exercise(
             name = "Push-up",
             equipment = "Bodyweight",
-            countType = "Reps",
+            countType = RepsAndWeight(null, 0.0),
             muscleGroup = "Chest"
         ),
         Exercise(
             name = "Dumbbell Squat",
             equipment = "Dumbbell",
-            countType = "Weight and Reps",
+            countType = RepsAndWeight(null, null),
             muscleGroup = "Legs"
         )
     )
---
+++ app/src/main/java/com/ignismark/castor/data/local/LocalWorkoutDataProvider.kt
@@ -0,0 +1,51 @@
+package com.ignismark.castor.data.local
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.ignismark.castor.model.Attempt
+import com.ignismark.castor.model.RepsAndWeight
+import com.ignismark.castor.model.Set
+import com.ignismark.castor.model.Workout
+import java.time.LocalDate
+import java.time.LocalTime
+import kotlin.time.Duration
+
+object LocalWorkoutDataProvider {
+    @RequiresApi(Build.VERSION_CODES.O)
+    val workout: Workout =
+        Workout(
+            date = LocalDate.of( 2025,  3,  19),
+            startTime = LocalTime.of(14,  30),
+            endTime = LocalTime.of(15,  18),
+            duration = Duration.parse("58"),
+            sets = listOf(
+                Set(
+                    rest = Duration.parse("10"),
+                    attempts = listOf(
+                        Attempt(
+                            exercise = LocalExercisesDataProvider.exercises[0],
+                            amount = RepsAndWeight(
+                                reps = 10,
+                                weight = 100.0
+                            )
+                        ),
+                        Attempt(
+                            exercise = LocalExercisesDataProvider.exercises[0],
+                            amount = RepsAndWeight(
+                                reps = 9,
+                                weight = 100.0
+                            )
+                        ),
+                        Attempt(
+                            exercise = LocalExercisesDataProvider.exercises[0],
+                            amount = RepsAndWeight(
+                                reps = 8,
+                                weight = 100.0
+                            )
+                        )
+                    )
+                )
+            )
+
+        )
+}
