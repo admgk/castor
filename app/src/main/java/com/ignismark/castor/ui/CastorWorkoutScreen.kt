9d12635240af5b48a6534fbdddf28dcc4a56c04b
admgk <adamgorz@gmail.com>
2025-03-25 18:15:59 +0100

    Add Set and Attempt Composables

--- app/src/main/java/com/ignismark/castor/ui/CastorWorkoutScreen.kt
+++ app/src/main/java/com/ignismark/castor/ui/CastorWorkoutScreen.kt
@@ -13,19 +13,19 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.ignismark.castor.data.local.LocalWorkoutDataProvider
+import com.ignismark.castor.model.Attempt
 import com.ignismark.castor.model.Workout
+import com.ignismark.castor.model.Set
 import com.ignismark.castor.utils.CastorContentType
 
 @Composable
@@ -48,40 +48,6 @@ fun CastorWorkoutScreen(
         Column {
             WorkoutInfoCard(workout = LocalWorkoutDataProvider.workout)
         }
-        /*Row(
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            LazyColumn(
-                modifier = Modifier.weight(1f),
-                contentPadding = PaddingValues(
-                    start = if (contentType == CastorContentType.ListAndDetails) 32.dp else 16.dp,
-                    top = 4.dp,
-                    end = 16.dp,
-                    bottom = 4.dp
-                )
-            ) {
-                items(castorUiState.exercisesAll) { exercise ->
-                    ExerciseCard(
-                        exercise = exercise,
-                        onClick = {
-                            castorViewModel.updateCurrentSelectedExercise(exercise)
-                            if (contentType == CastorContentType.ListOnly) {
-                                navController.navigate(CastorAppScreen.ExerciseDetails.title)
-                            }
-                        }
-                    )
-                }
-            }
-
-            if (contentType == CastorContentType.ListAndDetails) {
-                ExerciseDetailsCard(
-                    modifier = Modifier.weight(1f),
-                    exercise = castorUiState.currentSelectedExercise,
-                    contentType = contentType,
-                    navigateBack = { navController.navigate(CastorAppScreen.ExercisesList.title) }
-                )
-            }
-        }*/
     }
 }
 
@@ -118,5 +84,42 @@ fun WorkoutInfoCard(
                 )
             }
         }
+
+        LazyColumn {
+            items(workout.sets) { set ->
+                SetCard(set = set)
+            }
+        }
+    }
+}
+
+@Composable
+fun SetCard(
+    set: Set
+) {
+    Card(border = BorderStroke(1.dp, Color.LightGray),
+        elevation = CardDefaults.cardElevation(2.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 4.dp)
+    ) {
+        Column {
+            Text(
+                text = "SET, " + "Rest: ${set.rest}",
+                textAlign = TextAlign.Center,
+            )
+            set.attempts.forEach {
+                AttemptItem(attempt = it)
+            }
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun AttemptItem(attempt: Attempt) {
+    Column {
+        Text(text = attempt.exercise.name)
+        Text(text = attempt.amount.toString())
     }
 }
