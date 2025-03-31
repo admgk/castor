a1f48a8b89cc6dea913a94e54b95eeba4b7af0cc
admgk <adamgorz@gmail.com>
2025-03-31 17:13:35 +0200

    Add Log info message with workout data

--- app/src/main/java/com/ignismark/castor/model/Workout.kt
+++ app/src/main/java/com/ignismark/castor/model/Workout.kt
@@ -12,7 +12,18 @@ data class Workout(
     val workoutItems: List<WorkoutItem> = emptyList(),
     val duration: Duration,
     val intensity: Intensity
-)
+) {
+    override fun toString(): String {
+        return "workoutId: $workoutId\n" +
+                "userId: $userId\n" +
+                "date: $date\n" +
+                "name: $name\n" +
+                "notes: $notes\n" +
+                "workoutItems: ${workoutItems[2]}\n" +
+                "duration: $duration\n" +
+                "intensity: $intensity\n"
+    }
+}
 
 enum class Intensity {
     LOW, MEDIUM, HIGH
--- app/src/main/java/com/ignismark/castor/ui/CastorWorkoutScreen.kt
+++ app/src/main/java/com/ignismark/castor/ui/CastorWorkoutScreen.kt
@@ -1,5 +1,6 @@
 package com.ignismark.castor.ui
 
+import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
@@ -13,6 +14,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -81,6 +83,15 @@ fun WorkoutInfoCard(
             items(workout.workoutItems) { item ->
                 WorkoutItemCard(item = item)
             }
+        }
+
+        OutlinedButton(
+            onClick = { Log.i(
+                "Castor",
+                workout.toString())
+            }
+        ) {
+            Text(text = "Print Log's")
         }
     }
 }
